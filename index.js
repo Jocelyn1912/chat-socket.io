e09621e2b255895615e7a709091e633a169a48de
Jocelyn1912 <toty_2410@hotmail.com>
2018-02-16 18:25:04 -0300

    Chat grupal

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
+// Inicializando express para controlar funciones del servidor
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// '/' controlador de rutas para el sitio web
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html'); //Pasando el HTML
 });
+
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
+     io.emit('chat message', msg);
   });
 });
 
+// Haciendo que el servidor mire al puerto
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
